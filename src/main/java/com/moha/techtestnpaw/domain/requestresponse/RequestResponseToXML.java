519ca26eebc5cf00b69fb1a27adea6bd8682ed96
moha <mohamedbenmousa@gmail.com>
2018-08-28 21:36:53 +0200

    Removed standalone from xml writer

--- src/main/java/com/moha/techtestnpaw/domain/requestresponse/RequestResponseToXML.java
+++ src/main/java/com/moha/techtestnpaw/domain/requestresponse/RequestResponseToXML.java
@@ -14,9 +14,9 @@ public class RequestResponseToXML implements RequestResponseWriter {
     public String getResponse(RequestResponse requestResponse) throws JAXBException {
         JAXBContext jc = JAXBContext.newInstance(RequestResponse.class);
         Marshaller marshaller = jc.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
-        // REMOVE STANDALONE = YES DEL XML
+        marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
         StringWriter sw = new StringWriter();
+        sw.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
         marshaller.marshal(requestResponse, sw);
         return sw.toString();
     }
