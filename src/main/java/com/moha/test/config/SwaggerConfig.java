cd3cf6931727100b128fa9b3979a8ee2d0765123
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-08 22:32:15 +0000

    Deploy mohasaid/spring-boot-h2-hazelcast to github.com/mohasaid/spring-boot-h2-hazelcast.git:gh-pages

--- src/main/java/com/moha/test/config/SwaggerConfig.java
+++ src/main/java/com/moha/test/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -16,8 +20,21 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.moha.test.controllers"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo());
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring-boot-H2-Hazelcast API",
+                "API for handling HTTP service requests",
+                "1.0",
+                "Terms of service",
+                new Contact("Mohamed Said Benmousa", "https://mohasaid.github.io/about/", "mohamedbenmousa@gmail.com"),
+                "MIT", "https://opensource.org/licenses/MIT", Collections.emptyList());
+    }
+
 }
--- src/main/java/com/moha/test/controllers/RequestController.java
+++ src/main/java/com/moha/test/controllers/RequestController.java
@@ -5,21 +5,27 @@ import com.moha.test.domain.host.Host;
 import com.moha.test.domain.request.Request;
 import com.moha.test.domain.request.RequestBuilder;
 import com.moha.test.domain.request.RequestId;
+import com.moha.test.domain.requestresponse.RequestResponse;
 import com.moha.test.exceptions.BalancerException;
 import com.moha.test.services.BalancerService;
 import com.moha.test.services.RequestService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Api(tags = "Requests controller", value = "possible operations for requests")
 public class RequestController {
 
     private final RequestService requestService;
@@ -32,11 +38,28 @@ public class RequestController {
         this.balancerService = balancerService;
     }
 
+    @RequestMapping("/")
+    @ApiIgnore
+    public void home(HttpServletResponse httpResponse) throws IOException {
+        httpResponse.sendRedirect("/swagger-ui.html");
+    }
+
+    @ApiOperation(
+            value = "Returns data from a request",
+            notes = "Returns a xml which specifies the view code, the ping time and the cluster that will receive the information sent by the plugin.",
+            response = RequestResponse.class
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 204, message = "Data not found in database"),
+            @ApiResponse(code = 405, message = "Invalid input"),
+    })
     @GetMapping(value = "/getData", produces = MediaType.APPLICATION_XML_VALUE)
     @ResponseBody
-    public ResponseEntity getData(@RequestParam(value = "accountCode") String accountCode,
-                                  @RequestParam(value = "targetDevice") String targetDevice,
-                                  @RequestParam(value = "pluginVersion") String pluginVersion) throws BalancerException {
+    public ResponseEntity getData(@ApiParam(value = "account code of the request", required = true, example = "clienteA") @RequestParam(value = "accountCode") String accountCode,
+                                  @ApiParam(value = "target device of the request", required = true, example = "XBox") @RequestParam(value = "targetDevice") String targetDevice,
+                                  @ApiParam(value = "plugin version that is using", required = true, example = "3.3.1") @RequestParam(value = "pluginVersion") String pluginVersion)
+            throws BalancerException {
 
         Optional<Request> request = requestService.findById(new RequestId(accountCode, targetDevice, pluginVersion));
 
@@ -53,12 +76,22 @@ public class RequestController {
         }
     }
 
+    @ApiOperation(
+            value = "Adds a new request to the database",
+            notes = "Request object that needs to be added to the database."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 405, message = "Invalid input")
+    })
     @PostMapping(value = "/addData")
-    public void addData(@RequestParam(value = "accountCode") String accountCode,
-                        @RequestParam(value = "targetDevice") String targetDevice,
-                        @RequestParam(value = "pluginVersion") String pluginVersion,
-                        @RequestParam(value = "pingTime") String pingTime,
-                        @RequestParam(value = "hosts") String hosts) throws IOException {
+    @ResponseStatus
+    public ResponseEntity addData(@ApiParam(value = "account code of the request", required = true, example = "clienteC") @RequestParam(value = "accountCode") String accountCode,
+                                  @ApiParam(value = "target device of the request", required = true, example = "XBox") @RequestParam(value = "targetDevice") String targetDevice,
+                                  @ApiParam(value = "plugin version of the request", required = true, example = "3.3.1") @RequestParam(value = "pluginVersion") String pluginVersion,
+                                  @ApiParam(value = "ping time of the request", required = true, example = "10") @RequestParam(value = "pingTime") String pingTime,
+                                  @ApiParam(value = "hosts of the request", required = true, example = "[{\"name\":\"host-test\",\"load\":10}]") @RequestParam(value = "hosts") String hosts)
+            throws IOException {
 
         List<Host> hostList = Arrays.asList(objectMapper.readValue(hosts, Host[].class));
         final Request request = RequestBuilder.aRequest()
@@ -70,12 +103,23 @@ public class RequestController {
                 .build();
 
         requestService.save(request);
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
+
+    @ApiOperation(
+            value = "Deletes request data from database",
+            notes = "Request identifier that needs to be deleted from database."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 405, message = "Invalid input")}
+    )
     @PostMapping(value = "/deleteData")
-    public void deleteData(@RequestParam(value = "accountCode") String accountCode,
-                           @RequestParam(value = "targetDevice") String targetDevice,
-                           @RequestParam(value = "pluginVersion") String pluginVersion) {
+    public ResponseEntity deleteData(@ApiParam(value = "account code of the request", required = true, example = "clienteA") @RequestParam(value = "accountCode") String accountCode,
+                                     @ApiParam(value = "target device of the request", required = true, example = "XBox") @RequestParam(value = "targetDevice") String targetDevice,
+                                     @ApiParam(value = "plugin version of the request", required = true, example = "3.3.1") @RequestParam(value = "pluginVersion") String pluginVersion) {
         requestService.deleteById(new RequestId(accountCode, targetDevice, pluginVersion));
+        return ResponseEntity.ok().build();
     }
 }
