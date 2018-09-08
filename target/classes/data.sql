54b23566d9c2dacbe08184f39a76acee991bcb67
Moha <moha.said@rocketroi.com>
2018-09-08 17:57:20 +0200

    Remove target folder

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO REQUEST(ACCOUNT_CODE, TARGET_DEVICE, PLUGIN_VERSION, PING_TIME, HOSTS)
-VALUES ('clienteA', 'XBox', '3.3.1', 10, '[{"name":"clusterA.com", "load":70}, {"name":"clusterB.com", "load":30}]');
-
-INSERT INTO REQUEST(ACCOUNT_CODE, TARGET_DEVICE, PLUGIN_VERSION, PING_TIME, HOSTS)
-VALUES ('clienteA', 'Panasonic', '3.3.2', 5, '[{"name":"clusterA.com", "load":100}]');
-
-INSERT INTO REQUEST(ACCOUNT_CODE, TARGET_DEVICE, PLUGIN_VERSION, PING_TIME, HOSTS)
-VALUES ('clienteB', 'osmf', '3.3.1', 5, '[{"name":"clusterA.com", "load":50}, {"name":"clusterB.com", "load":50}]');
